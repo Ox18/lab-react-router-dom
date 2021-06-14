912e9da021460b017b1e5b4b70fd5d0720bb4489
Alex <Wilmer.DelgadoAlama09@gmail.com>
2021-06-13 23:02:02 -0500

    change url useroutematch on subswitch

--- src/routers/CategoriesRouter.js
+++ src/routers/CategoriesRouter.js
@@ -1,29 +1,31 @@
-import { NavLink, Switch, Route, Redirect } from "react-router-dom";
+import { NavLink, Switch, Route, Redirect, useRouteMatch } from "react-router-dom";
 
 import Categories from "../pages/CategoriesPage";
 import PrivateRoute from "./PrivateRoute";
 // test
 export default function CategoriesRouter() {
+  const { url } = useRouteMatch();
+  
   return (
     <div>
       <ul>
         <li>
-          <NavLink activeClassName="active" exact to="/categories">
+          <NavLink activeClassName="active" exact to={`${url}`}>
             All
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="active" exact to="/categories/terror">
+          <NavLink activeClassName="active" exact to={`${url}/terror`}>
             Terror
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="active" exact to="/categories/action">
+          <NavLink activeClassName="active" exact to={`${url}/action`}>
             Action
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="active" exact to="/categories/anime">
+          <NavLink activeClassName="active" exact to={`${url}/anime`}>
             Anime
           </NavLink>
         </li>
